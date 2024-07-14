68083c5e9027b7c9dfaeee72d0b59d2349bc9d8f
fatmabadawy <143001487+fatmabadawy@users.noreply.github.com>
2024-07-15 00:41:32 +0300

    Add files via upload

---
+++ hos.js
@@ -0,0 +1,8 @@
+function show() {
+    const sidebar = document.querySelector('.sidebar') 
+    sidebar.style.display = 'flex'; 
+  }
+  function hide() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'; 
+  }
